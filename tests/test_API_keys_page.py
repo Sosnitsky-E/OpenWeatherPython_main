a0b1bec1dcc3ff8787422aba361d260623b40d17
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-06-15 10:06:16 +0300

    test_tc_017_02_09_the_icon_activate_api_key_is_displayed added

--- tests/test_API_keys_page.py
+++ tests/test_API_keys_page.py
@@ -55,6 +55,11 @@ class TestApiKey:
         api_keys_page.open_api_keys_page()
         api_keys_page.check_is_icon_deactivate_api_key_displayed()
 
+    def test_tc_017_02_09_the_icon_activate_api_key_is_displayed(self, driver):
+        api_keys_page = ApiKeysPage(driver)
+        api_keys_page.open_api_keys_page()
+        api_keys_page.check_is_icon_activate_api_key_displayed()
+
     def test_tc_017_02_08_the_active_status_of_api_key_is_black(self, driver):
         row_num = 1
         api_keys_page = ApiKeysPage(driver)
