028a68ad58a339ec52cd69643131ccb2fdbf383a
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-06-19 09:31:10 +0300

    test_tc_017_02_10_the_alert_for_changing_api_key_status_is_displayed added

--- pages/API_keys_page.py
+++ pages/API_keys_page.py
@@ -13,10 +13,6 @@ class ApiKeysPage(BasePage):
         api_key_tab = self.driver.find_element(*ApiKeysLocator.TAB_API_KEYS)
         api_key_tab.click()
 
-    def check_module_title_create_api_key_is_visible(self):
-        module_create_api_key = self.driver.find_element(*ApiKeysLocator.MODULE_API_KEY_CREATE)
-        assert module_create_api_key.is_displayed(), "module with title “Create key“ does not visible"
-
     def open_popup_rename_api_key(self):
         edit_api_key_icon = self.driver.find_element(*ApiKeysLocator.EDIT_API_KEY_ICON)
         edit_api_key_icon.click()
@@ -35,31 +31,9 @@ class ApiKeysPage(BasePage):
     def api_key_name_of_first_row(self):
         return self.driver.find_element(*ApiKeysLocator.API_KEY_NAME_FIRST_ROW).text
 
-    def check_limit_of_api_key_name(self):
-        api_name_limit = 20
-        actual_length_of_api_key_name = len(self.api_key_name_of_first_row())
-        assert actual_length_of_api_key_name == api_name_limit, "The limit of API key name does not correspond to " \
-                                                                "expected limit"
-
     def enter_created_api_key_name(self, new_api_name):
         new_api_key_name_field = self.driver.find_element(*ApiKeysLocator.NEW_API_KEY_NAME)
         new_api_key_name_field.send_keys(new_api_name)
-
-    def check_limit_of_created_api_key_name(self):
-        expected_api_name_limit = 20
-        created_api_key_name = self.driver.find_element(*ApiKeysLocator.NEW_API_KEY_NAME)
-        actual_api_name_limit = int(created_api_key_name.get_attribute('maxlength'))
-        assert actual_api_name_limit == expected_api_name_limit, "The limit of created API key name does not correspond to " \
-                                                                 "requirements"
-
-    def check_create_api_key_field_is_required(self):
-        new_api_key_name_field = self.driver.find_element(*ApiKeysLocator.NEW_API_KEY_NAME)
-        is_required = new_api_key_name_field.get_attribute('required')
-        assert is_required, "The field hasn't required attribute"
-
-    def check_is_generate_button_clickable(self):
-        is_generate_button_clickable = self.element_is_clickable(ApiKeysLocator.GENERATE_BUTTON)
-        assert is_generate_button_clickable, "The button does not clickable"
 
     def click_generate_api_key_name_button(self):
         generate_api_key_button = self.driver.find_element(*ApiKeysLocator.GENERATE_BUTTON)
@@ -68,16 +42,6 @@ class ApiKeysPage(BasePage):
     def get_length_of_table_api_keys(self):
         initial_table_api_keys = self.elements_are_visible(ApiKeysLocator.TABLE_API_KEYS_CONTENT)
         return len(initial_table_api_keys)
-
-    def check_is_api_key_generated(self, initial_table_length):
-        actual_api_keys_table_length = self.get_length_of_table_api_keys()
-        assert actual_api_keys_table_length == initial_table_length + 1, "The new API key does not generated"
-
-    def check_is_success_generate_notice_displayed(self):
-        expected_new_api_key_notice_text = 'API key was created successfully'
-        actual_new_api_key_notice_text = self.driver.find_element(*ApiKeysLocator.NOTICE_MESSAGE).text
-        assert actual_new_api_key_notice_text == expected_new_api_key_notice_text, \
-            "The success generate API key notice text does not displayed"
 
     def get_last_row_elements_from_api_keys_table(self):
         len_t = self.get_length_of_table_api_keys()
@@ -107,6 +71,42 @@ class ApiKeysPage(BasePage):
             alert = self.driver.switch_to.alert
             alert.accept()
 
+    def check_module_title_create_api_key_is_visible(self):
+        module_create_api_key = self.driver.find_element(*ApiKeysLocator.MODULE_API_KEY_CREATE)
+        assert module_create_api_key.is_displayed(), "module with title “Create key“ does not visible"
+
+    def check_limit_of_api_key_name(self):
+        api_name_limit = 20
+        actual_length_of_api_key_name = len(self.api_key_name_of_first_row())
+        assert actual_length_of_api_key_name == api_name_limit, "The limit of API key name does not correspond to " \
+                                                                "expected limit"
+
+    def check_limit_of_created_api_key_name(self):
+        expected_api_name_limit = 20
+        created_api_key_name = self.driver.find_element(*ApiKeysLocator.NEW_API_KEY_NAME)
+        actual_api_name_limit = int(created_api_key_name.get_attribute('maxlength'))
+        assert actual_api_name_limit == expected_api_name_limit, "The limit of created API key name does not correspond to " \
+                                                                 "requirements"
+
+    def check_create_api_key_field_is_required(self):
+        new_api_key_name_field = self.driver.find_element(*ApiKeysLocator.NEW_API_KEY_NAME)
+        is_required = new_api_key_name_field.get_attribute('required')
+        assert is_required, "The field hasn't required attribute"
+
+    def check_is_generate_button_clickable(self):
+        is_generate_button_clickable = self.element_is_clickable(ApiKeysLocator.GENERATE_BUTTON)
+        assert is_generate_button_clickable, "The button does not clickable"
+
+    def check_is_api_key_generated(self, initial_table_length):
+        actual_api_keys_table_length = self.get_length_of_table_api_keys()
+        assert actual_api_keys_table_length == initial_table_length + 1, "The new API key does not generated"
+
+    def check_is_success_generate_notice_displayed(self):
+        expected_new_api_key_notice_text = 'API key was created successfully'
+        actual_new_api_key_notice_text = self.driver.find_element(*ApiKeysLocator.NOTICE_MESSAGE).text
+        assert actual_new_api_key_notice_text == expected_new_api_key_notice_text, \
+            "The success generate API key notice text does not displayed"
+
     def check_default_status_generated_api_key(self):
         last_row_elements = self.get_last_row_elements_from_api_keys_table()
         new_generated_api_key_status = last_row_elements[2].text
@@ -117,7 +117,7 @@ class ApiKeysPage(BasePage):
         initial_view_api_key_tab = api_key_tab_elements[2].get_attribute('class')
         assert initial_view_api_key_tab == "active", "API Keys tab view is not active"
 
-    def check_is_alert_info_displayed(self):
+    def check_is_main_alert_info_displayed(self):
         alert_info_displayed = self.element_is_displayed(ApiKeysLocator.ALERT_INFO)
         assert alert_info_displayed, "Alert info does not displayed"
 
@@ -174,3 +174,11 @@ class ApiKeysPage(BasePage):
     def check_change_api_key_status_icon_is_clickable(self):
         change_api_key_status_icon_clickable = self.element_is_clickable(ApiKeysLocator.CHANGE_API_KEY_STATUS_ICON)
         assert change_api_key_status_icon_clickable, "The icon for changing API key status is not clickable"
+
+    def check_alert_for_confirming_change_api_key_status_is_displayed(self):
+        expected_alert_text = 'Do you want to deactivate this key?'
+        change_api_key_status_icon = self.element_is_clickable(ApiKeysLocator.CHANGE_API_KEY_STATUS_ICON)
+        change_api_key_status_icon.click()
+        alert = self.driver.switch_to.alert
+        actual_alert_text = alert.text
+        assert actual_alert_text == expected_alert_text, "The alert text does not correspond to expected"
