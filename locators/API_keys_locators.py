ddae0b0918cb74e152723e268a7e6d184427c456
Elena Sosnitsky <sosnitsky.e@gmail.com>
2023-06-22 07:36:47 +0300

    test_tc_017_03_01_the_api_key_name_is_displayed added

--- locators/API_keys_locators.py
+++ locators/API_keys_locators.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 
 class ApiKeysLocator:
     API_KEY_NAME_URL = 'https://home.openweathermap.org/api_keys'
-    API_KEY_EDIT_SELECTOR = By.CSS_SELECTOR, "i[class='fa fa-edit']"
-    API_KEY_NAME_SELECTOR = By.XPATH, "//table/tbody/tr/td[2]"
+    API_KEY_EDIT_ICON = By.CSS_SELECTOR, "i[class='fa fa-edit']"
+    API_KEY_NAME = By.XPATH, "//table/tbody/tr/td[2]"
     API_KEY_ENTER_SELECTOR = By.CSS_SELECTOR, "input[name='edit_key_form[name]']"
     SAVE_BUTTON_SELECTOR = By.CSS_SELECTOR, "button[class='button-round dark']"
     TAB_API_KEYS = By.CSS_SELECTOR, '#myTab [href="/api_keys"]'
